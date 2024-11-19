5c7888ccaa75aab0675b7a36fe72da993d65d9d9
Hezekiahs <111096175+HezekiahsDev@users.noreply.github.com>
2024-11-19 07:43:52 +0100

    Delete public directory

--- public/index.js
+++
@@ -1,3 +0,0 @@
-export { default as GoogleStore } from "./images/GoogleStore.svg";
-export { default as AppleStore } from "./images/AppleStore.svg";
-export { default as HeroBackground } from "./images/HeroBackground.png";
